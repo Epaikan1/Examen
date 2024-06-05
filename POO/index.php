5f2e83d02594311667711e602ba72e8ba9dce992
edris paikan <paikanadris@gmail.com>
2024-06-05 12:39:05 +0200

    Update index.php
    creation le ficher pour lancer le projet

---
+++ POO/index.php
@@ -0,0 +1,11 @@
+<?php
+include_once 'Individu.php';
+include_once 'Etudiant.php';
+include_once 'Enseignant.php';
+include_once 'Cours.php';
+include_once 'Administrateur.php';
+include_once 'EtudiantAlertable.php';
+
+
+
+?>
