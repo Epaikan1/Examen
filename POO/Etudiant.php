52fa6d6de5e092c6356fe70cf8e8e1af28b22ac2
edris paikan <paikanadris@gmail.com>
2024-06-05 12:47:48 +0200

    Création de index.php
    Index.php est pour tester le projet
    
    et ajouter des autres element dans le class etudiant que je trouve utile pour cela

--- POO/Etudiant.php
+++ POO/Etudiant.php
@@ -4,11 +4,13 @@ include_once 'Individu.php';
 class Etudiant extends Individu {
     private $dateInscription;
     private $niveauEtude;
+    private $coursParticipes;
 
     public function __construct($nom, $prenom, $identifiant, $dateInscription, $niveauEtude) {
         parent::__construct($nom, $prenom, $identifiant);
         $this->dateInscription = $dateInscription;
         $this->niveauEtude = $niveauEtude;
+        $this->coursParticipes = [];
     }
 
     public function getDateInscription() {
@@ -26,11 +28,22 @@ class Etudiant extends Individu {
     public function setNiveauEtude($niveauEtude) {
         $this->niveauEtude = $niveauEtude;
     }
+
     public function participer($cours) {
         if ($cours->getPlacesRestantes() > 0) {
+            $this->coursParticipes[] = $cours;
             $cours->setPlacesRestantes($cours->getPlacesRestantes() - 1);
         } else {
             echo "Plus de places disponibles pour le cours " . $cours->getTitre() . "<br>";
+        }
+    }
+
+    public function quitter($cours) {
+        if (($key = array_search($cours, $this->coursParticipes)) !== false) {
+            unset($this->coursParticipes[$key]);
+            $cours->setPlacesRestantes($cours->getPlacesRestantes() + 1);
+        } else {
+            echo "L'étudiant n'est pas inscrit à ce cours<br>";
         }
     }
 
--- POO/index.php
+++ POO/index.php
@@ -6,6 +6,29 @@ include_once 'Cours.php';
 include_once 'Administrateur.php';
 include_once 'EtudiantAlertable.php';
 
+$individus = [];
+$individus[] = new Etudiant("Dupont", "Jean", 1, date("Y-m-d"), "Licence");
+$individus[] = new Enseignant("Martin", "Pierre", 2, "Math?matiques", 2500.00);
 
+foreach ($individus as $individu) {
+    $individu->montrerDetails();
+}
+// Exemple de doneées cours
+$cours1 = new Cours("Informatique", "Dr. Smith", 40, 30);
+$cours1 = new Cours("Informatique", "Dr. Smith", 40, 30);
+$cours1 = new Cours("Informatique", "Dr. Smith", 40, 30);
+$cours1 = new Cours("Informatique", "Dr. Smith", 40, 30);
+$etudiant = $individus[0];
+$etudiant->participer($cours1);
+$cours1->montrerDetails();
+$etudiant->quitter($cours1);
+$cours1->montrerDetails();
+// Exemple de doneées Administrateur dont prof
+$admin = new Administrateur("Admin", "Admin", 3, "Gestion", 3000.00);
+// Exemple de doneées Etudiant
 
+$etudiantAlertable = new EtudiantAlertable("Dupont", "Jean", 1, date("Y-m-d"), "Licence");
+
+$admin->alerterAnnulation($cours1);
+$etudiantAlertable->alerterAnnulation($cours1);
 ?>
